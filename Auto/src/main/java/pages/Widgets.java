10cefee9c4a5d6eb7825a0633b25a9db5353885e
ManuelAmoresGonzalez <amoresgonzalezmanuel@gmail.com>
2022-05-07 19:41:23 -0600

    Select Menu test

--- Auto/src/main/java/pages/Widgets.java
+++ Auto/src/main/java/pages/Widgets.java
@@ -1,14 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class Widgets extends BasePage{
     @FindBy(xpath = "//div[contains(text(),'Widgets')]")
     private WebElement button_Widgets;
+
+    @FindBy(xpath = "//div[contains(@class,'select-menu-container')]")
+    private WebElement boxSelectMenu;
+
+    @FindBy(xpath = "//input[contains(@id,'react-select-2-input')]")
+    private WebElement menuSelectValue;
+
+
+    @FindBy(xpath = "//div[contains(@class,'css-1hwfws3')]")
+    private WebElement selectValueMultiselect;
 
     @FindBy(id = "item-2")
     private WebElement button_dataPicker;
@@ -17,10 +29,17 @@ public class Widgets extends BasePage{
         PageFactory.initElements(driver,this);
     }
 
-    public boolean uploadFile(){
-        scrollPage();
-        button_Widgets.click();
-        button_dataPicker.click();
+    public boolean selectMenuMethod(){
+        boxSelectMenu.findElement(By.xpath("//div[contains(@class,' css-1hwfws3')]")).click();
+        driver.findElement(By.xpath("//div[contains(@id,'react-select-2-option-1-0')]")).click();
+        boxSelectMenu.findElement(By.xpath("//div[contains(@class,' css-1hwfws3')]")).click();
+        Select se = new Select(driver.findElement(By.xpath("//*[@id='oldSelectMenu']")));
+        se.selectByValue("6");
+        System.out.println(selectValueMultiselect);
+        selectValueMultiselect.findElement(By.xpath("//div[contains(text(),'Select...')]")).click();
+        driver.findElement(By.xpath("//div[contains(@id,'react-select-4-option-0')]")).click();
+        driver.findElement(By.xpath("//div[contains(@id,'react-select-4-option-2')]")).click();
+        selectValueMultiselect.findElement(By.xpath("//div[contains(text(),'Select...')]")).click();
         return true;
     }
 
---
+++ Auto/src/main/java/tests/WidgetsTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.BasePage;
+import pages.Widgets;
+
+public class WidgetsTest {
+
+    WebDriver driver;
+    BasePage basePage;
+
+    @BeforeMethod
+    private void setUp(){
+        WebDriverManager.chromedriver().browserVersion("100.0.4896.60").setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("enable-automation");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-browser-side-navigation");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--allowed-ips");
+        driver = new ChromeDriver(options);
+        basePage = new BasePage(driver);
+        driver.get("https://demoqa.com/select-menu");
+    }
+    @Test
+    private void test10(){
+        Widgets element= new Widgets(driver);
+        element.selectMenuMethod();
+        //Assert.assertTrue(element.dragDrop(true));
+    }
+}
